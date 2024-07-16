9b8077cefde8056da6cec9791dd4afdb1252d1a1
redhed100 <redhed.volodin@gmail.com>
2024-07-16 17:43:33 +0300

    Removed dir in snake VecDeque

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,16 @@ enum Dir {
     //None
 }
 
+#[derive(Copy, Clone)]
+enum Var {
+    Lr,
+    Ud,
+    Lu,
+    Ld,
+    Ur,
+    Dr
+}
+
 // impl Dir {
 //     pub fn clone(&self) -> Dir {
 //         match self {
@@ -57,7 +67,7 @@ impl Game {
 }
 
 struct Snake {
-    snake: VecDeque<(Pos, Dir)>,
+    snake: VecDeque<Pos>,
     dir: Dir
     // headX: u16,
     // headY: u16
@@ -66,23 +76,53 @@ struct Snake {
 impl Snake {
     pub fn new(x: u16, y: u16) -> Snake {
         let mut snake = Snake { snake: VecDeque::new(), dir: Dir::Right};
-        snake.snake.push_back((Pos::new(x-2, y), Dir::Right));
-        snake.snake.push_back((Pos::new(x-1, y), Dir::Right));
-        snake.snake.push_back((Pos::new(x, y), Dir::Right));
+        snake.snake.push_back(Pos::new(x-2, y));
+        snake.snake.push_back(Pos::new(x-1, y));
+        snake.snake.push_back(Pos::new(x, y));
 
         snake
     }
 
     fn head_x(&self) -> u16 {
-        self.snake.back().unwrap().0.x
+        self.snake.back().unwrap().x
     }
 
     fn head_y(&self) -> u16 {
-        self.snake.back().unwrap().0.y
+        self.snake.back().unwrap().y
     }
 
     pub fn get_dir(&self) -> Dir {
         self.dir //.clone()
+    }
+
+    pub fn get_var(&self, i: usize) -> Var {
+        if self.sget(i-1).x+1 == self.sget(i).x && self.sget(i+1).x-1 == self.sget(i).x ||
+           self.sget(i-1).x-1 == self.sget(i).x && self.sget(i+1).x+1 == self.sget(i).x {
+            Var::Lr // Left - Right
+        }
+        else if self.sget(i-1).y+1 == self.sget(i).y && self.sget(i+1).y-1 == self.sget(i).y ||
+                self.sget(i-1).y-1 == self.sget(i).y && self.sget(i+1).y+1 == self.sget(i).y {
+            Var::Ud // Up - Down
+        }
+        else if self.sget(i-1).x+1 == self.sget(i).x && self.sget(i+1).y+1 == self.sget(i).y ||
+                self.sget(i-1).y+1 == self.sget(i).y && self.sget(i+1).x+1 == self.sget(i).y  {
+            Var::Lu // Left - Up
+        }
+        else if self.sget(i-1).x+1 == self.sget(i).x && self.sget(i+1).y-1 == self.sget(i).y ||
+                self.sget(i-1).y-1 == self.sget(i).y && self.sget(i+1).x+1 == self.sget(i).x {
+            Var::Ld // Left - Down
+        }
+        else if self.sget(i-1).y+1 == self.sget(i).y && self.sget(i+1).x-1 == self.sget(i).x ||
+                self.sget(i-1).x-1 == self.sget(i).x && self.sget(i+1).y+1 == self.sget(i).y {
+            Var::Ur // Up - Right
+        }
+        else {
+            Var::Dr // Down - Right
+        }
+    }
+
+    pub fn sget(&self, i: usize) -> Pos {
+        *self.snake.get(i).unwrap()
     }
 
     pub fn is_dead(&self, map: &Map) -> bool {
@@ -100,8 +140,8 @@ impl Snake {
     //     }
     // }
 
-    pub fn move_full(&mut self) -> (Pos, Dir) { // выдает Pos и Dir бывшего хвоста
-        let mut p = self.snake.back().unwrap().0;
+    pub fn move_full(&mut self) -> Pos { // выдает Pos и Dir бывшего хвоста
+        let mut p = *self.snake.back().unwrap();
         match self.dir {
             Dir::Up => { p.y -= 1 },
             Dir::Left => { p.x -= 1 },
@@ -110,7 +150,7 @@ impl Snake {
             //_ => {}
         }
         // self.move_head();
-        self.snake.push_back((p, self.dir.clone()));
+        self.snake.push_back(p);
         self.snake.pop_front().unwrap()
     }
 
@@ -123,11 +163,11 @@ impl Snake {
     pub fn display_full(&self) {
         execute!(stdout(), SetForegroundColor(Color::Rgb { r: 66, g: 111, b: 227 })).unwrap();
         let mut last_pix = Pos::new(0, 0);
-        for (i, (pSnakePos, pSnakeDir)) in self.snake.iter().enumerate() {
+        for (i, pSnakePos) in self.snake.iter().enumerate() {
             if (pSnakePos.x+pSnakePos.y) % 2 == 0 { execute!(stdout(), SetBackgroundColor(Color::Rgb { r: 170, g: 215, b: 81 })).unwrap(); }
             else { execute!(stdout(), SetBackgroundColor(Color::Rgb { r: 162, g: 209, b: 73 })).unwrap(); }
             if i == 0 {
-                match pSnakeDir {
+                match self.dir {
                     Dir::Up => {
                         execute!(stdout(),
                             cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1),
@@ -156,56 +196,47 @@ impl Snake {
                 }
             }
             else if i > 0 && i < self.snake.len() - 1 {
-                match pSnakeDir {
-                    Dir::Up => {
-                        if last_pix.x == pSnakePos.x - 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("╧┘")).unwrap();
-                        }
-                        else if last_pix.x == pSnakePos.x + 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("└╧")).unwrap();
-                        }
-                        else if last_pix.y == pSnakePos.y + 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("││")).unwrap();
-                        }
+                match self.get_var(i) {
+                    Var::Lr => {
+                        execute!(stdout(),
+                            cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1),
+                            style::Print("══")
+                        ).unwrap();
                     },
-                    Dir::Left => {
-                        if last_pix.y == pSnakePos.y - 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("╧┘")).unwrap();
-                        }
-                        else if last_pix.x == pSnakePos.x + 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("══")).unwrap();
-                        }
-                        else if last_pix.y == pSnakePos.y + 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("╤┐")).unwrap();
-                        }
+                    Var::Ud => {
+                        execute!(stdout(),
+                            cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1),
+                            style::Print("││")
+                        ).unwrap();
                     },
-                    Dir::Down => {
-                        if last_pix.x == pSnakePos.x - 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("╤┐")).unwrap();
-                        }
-                        else if last_pix.y == pSnakePos.y - 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("││")).unwrap();
-                        }
-                        else if last_pix.x == pSnakePos.x + 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("┌╤")).unwrap();
-                        }
+                    Var::Lu => {
+                        execute!(stdout(),
+                            cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1),
+                            style::Print("╧┘")
+                        ).unwrap();
                     },
-                    Dir::Right => {
-                        if last_pix.x == pSnakePos.x - 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("══")).unwrap();
-                        }
-                        else if last_pix.y == pSnakePos.y - 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("└╧")).unwrap();
-                        }
-                        else if last_pix.y == pSnakePos.y + 1 {
-                            execute!(stdout(), cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1), style::Print("┌╤")).unwrap();
-                        }
+                    Var::Ld => {
+                        execute!(stdout(),
+                            cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1),
+                            style::Print("╤┐")
+                    ).unwrap();
                     },
-                    //_ => {}
+                    Var::Ur => {
+                        execute!(stdout(),
+                            cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1),
+                            style::Print("└╧")
+                        ).unwrap();
+                    },
+                    Var::Dr => {
+                        execute!(stdout(),
+                            cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1),
+                            style::Print("┌╤")
+                        ).unwrap();
+                    }
                 }
             }
             else /* if i >= self.snake.len() - 1 */ {
-                match pSnakeDir {
+                match self.dir {
                     Dir::Up => {
                         execute!(stdout(),
                             cursor::MoveTo(pSnakePos.x*2+1,pSnakePos.y+1),
@@ -262,7 +293,7 @@ darkBG:  Rgb { r: 162, g: 209, b: 73 }
         */
     }
 
-    pub fn display(&self, last_pix: (Pos, Dir), is_dead: bool) {
+    pub fn display(&self, last_pix: Pos, is_dead: bool) {
 
     }
 }
@@ -388,6 +419,6 @@ fn main() {
 
         let FPS = 1.0/fElapsedTime;
         let s = "FPS: ".to_string() + FPS.to_string().as_str();
-        execute!(stdout(), crossterm::terminal::SetTitle(s));
+        execute!(stdout(), crossterm::terminal::SetTitle(s)).unwrap();
     }
 }
